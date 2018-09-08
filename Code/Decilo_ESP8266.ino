d2e1ac3daf46084469d208cd91059a82408cb1fa
cirujadigital <maximilianoosito@gmail.com>
2018-09-08 01:38:05 -0300

    Add files via upload

---
+++ Code/Decilo_ESP8266.ino
@@ -0,0 +1,207 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include "spiffs/spiffs.h"
+#include <FS.h>
+
+//#include <Arduino.h>
+
+#include "AudioFileSourceSPIFFS.h"
+#include "AudioFileSourceID3.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2SNoDAC.h"
+AudioGeneratorMP3 *mp3;
+AudioFileSourceSPIFFS *file;
+AudioOutputI2SNoDAC *out;
+AudioFileSourceID3 *id3;
+
+boolean modo_carga;
+const char* ssid = "DECILOV2";
+const char* password = "";
+
+ int valor_analog_pin;
+
+ESP8266WebServer server(80);
+const char* serverIndex = "<form method='POST' action='/upload' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+File fsUploadFile;
+
+void handleFileUpload()
+{
+  if(server.uri() != "/upload") return;
+  HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    String filename = upload.filename;
+    if(!filename.startsWith("/")) filename = "/"+filename;
+   // Serial.print("handleFileUpload Name: "); 
+   // Serial.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");
+    filename = String();
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+   // Serial.print("handleFileUpload Data: "); 
+  //  Serial.println(upload.currentSize);
+    if(fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize);
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(fsUploadFile)
+      fsUploadFile.close();
+   // Serial.print("handleFileUpload Size: ");
+   // Serial.println(upload.totalSize);
+  }
+}
+
+void handleListFiles()
+{
+  String FileList = "File List:\n";
+  
+  Dir dir = SPIFFS.openDir("/");
+ // Serial.println("File List:");
+  while (dir.next())
+  {
+    String FileName = dir.fileName();
+    File f = dir.openFile("r");
+    String FileSize = String(f.size());
+    int whsp = 6-FileSize.length();
+    while(whsp-->0)
+    {
+   //   Serial.print(" ");
+      FileList += " ";
+    }
+  //  Serial.println(FileSize+" "+FileName);
+    FileList += FileSize+" "+FileName+"\n";
+  }     
+  server.sendHeader("Connection", "close");
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "text/plain", FileList);
+}
+
+void MDCallback(void *cbData, const char *type, bool isUnicode, const char *string)
+{
+  (void)cbData;
+ // Serial.printf("ID3 callback for: %s = '", type);
+
+  if (isUnicode) {
+    string += 2;
+  }
+  
+  while (*string) {
+    char a = *(string++);
+    if (isUnicode) {
+      string++;
+    }
+//    Serial.printf("%c", a);
+  }
+//  Serial.printf("'\n");
+//  Serial.flush();
+}
+
+void setup(void)
+{ //WiFi.forceSleepBegin();
+ WiFi.mode(WIFI_OFF);
+  if (analogRead(A0)<=11){
+  modo_carga= true; // habria que poner un interruptor para activar el modo carga.
+  
+  }
+  else
+  {
+    modo_carga=false;
+ 
+    }
+   // pinMode(D2,OUTPUT);
+  SPIFFS.begin();
+  pinMode(D1, OUTPUT); //LED
+  Serial.begin(115200);
+  digitalWrite(D1, HIGH);
+  
+  if (modo_carga)
+  {
+  Serial.println("Welcome to WebServer File System");
+  Serial.println("");
+  WiFi.mode(WIFI_AP);
+ // WiFi.begin(ssid, password);
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  //if (WiFi.waitForConnectResult() == WL_CONNECTED)
+  if(true)
+  {
+   // Serial.print("Local IP: ");
+   // Serial.println(myIP);
+    server.on("/", HTTP_GET, []() {
+      server.sendHeader("Connection", "close");
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(200, "text/html", serverIndex);
+    });
+    server.on("/list", HTTP_GET, handleListFiles);
+    server.onFileUpload(handleFileUpload);
+    server.on("/upload", HTTP_POST, []() {
+      server.sendHeader("Connection", "close");
+      server.sendHeader("Access-Control-Allow-Origin", "*");
+      server.send(200, "text/plain", "OK");
+    });
+    server.begin();
+  }
+  }
+  file = new AudioFileSourceSPIFFS("/funcion0.mp3");
+    id3 = new AudioFileSourceID3(file);
+  id3->RegisterMetadataCB(MDCallback, (void*)"ID3TAG");
+  out = new AudioOutputI2SNoDAC();
+  mp3 = new AudioGeneratorMP3();
+  mp3->begin(id3, out);
+ 
+}
+
+void loop(void) {
+  if(modo_carga)
+  {
+  server.handleClient();
+  }
+
+ else{
+  if (mp3->isRunning()) {
+    digitalWrite(D1, HIGH);
+    if (!mp3->loop())   mp3->stop(); digitalWrite(D1, LOW);   //delete file; delete id3; delete out; delete mp3;
+  }
+  else{
+       valor_analog_pin= analogRead(A0);
+//  Serial.println(valor_analog_pin);
+   
+   rep_mp3(0,11, "/funcion1.mp3");
+   rep_mp3(85,110,"/funcion2.mp3");
+
+   rep_mp3(160,185, "/funcion3.mp3");
+   rep_mp3(320,340,"/funcion4.mp3");
+
+   rep_mp3(270,290, "/funcion5.mp3");
+   rep_mp3(230,250,"/funcion6.mp3");
+
+   rep_mp3(350,370, "/funcion7.mp3");
+     rep_mp3(380,395,"/funcion8.mp3");
+
+   rep_mp3(405,430, "/funcion9.mp3");
+
+
+  
+  }
+  }
+}
+
+void rep_mp3( int analog_min, int analog_max, const char *nombre_archivo)
+{
+//  valor_analog_pin= analogRead(A0);
+// Serial.println(valor_analog_pin);
+  if(valor_analog_pin<=analog_max && valor_analog_pin>= analog_min){
+    delete file; delete id3; delete out; delete mp3;
+   
+   file = new AudioFileSourceSPIFFS(nombre_archivo);
+    id3 = new AudioFileSourceID3(file);
+  id3->RegisterMetadataCB(MDCallback, (void*)"ID3TAG");
+  out = new AudioOutputI2SNoDAC();
+  mp3 = new AudioGeneratorMP3();
+    
+  mp3->begin(id3, out);
+  
+  }
+ 
+  }
+
+
---
+++ Code/isd1760-arduinopromicro.ino
@@ -0,0 +1,139 @@
+//Decilo! V2- ISD1760+Arduino Pro Mini Version
+
+#include <ISD1700.h> //Librería para usar el ISD1700 de forma más sencilla. Disponible en: https://github.com/georgepatterson/ISD17xx/blob/master/sketch.pde
+
+//IMPORTANTE: MODIFICAR EN LA LIBRERIA LA LINEA -tanto en el archivo .h como en el .cpp #include Wprogram.h por #include Arduino.h 
+
+ISD1700 chip(10); //Pines SPI: 10 SS; 11 MOSI, 12 MISO, 13 SCK - Arduino UNO/NANO/PRO MINI 
+uint16_t playback_ptr[9]; //Modificar según la cantidad de botones
+uint16_t rec_ptr[9]; //Modificar según la cantidad de botones
+byte num_msj= 9; //numero de mensajes máximos posibles. Modificar según la cantidad de botones
+byte pos = 0;
+uint16_t pos0 = 0x010;
+
+int boton;
+int boton_previo=0;
+int boton_apretado;
+
+
+boolean primer_ciclo_grabado;
+
+int lectura_analog;
+
+boolean boton_presionado=false;;
+void setup()
+{
+
+  primer_ciclo_grabado= true;
+  playback_ptr[0]= pos0;
+  chip.pu();
+  chip.reset();
+  delay(100);
+  Serial.begin(9600);
+  Serial.println("Decilo! v2. ISD1760 Version. 2018 CirujaDigital");
+
+
+}
+
+void loop()
+{
+  
+if (primer_ciclo_grabado)
+{
+chip.pu();
+leer_playbackpointer();
+delay(200);
+primer_ciclo_grabado=false;
+}
+
+
+  lectura_analog= analogRead(A0);
+
+  
+  if(lectura_analog==0) {boton=1;}
+  if(lectura_analog>=88 && lectura_analog<=93) {boton=2; }
+  if(lectura_analog>=168 && lectura_analog<=171) {boton=3;}
+  if(lectura_analog>=336 && lectura_analog<=342) {boton=4;}
+  if(lectura_analog>=288 && lectura_analog<=292) {boton=5;}
+  if(lectura_analog>=230 && lectura_analog<=236) {boton=6;}
+  if(lectura_analog>=378 && lectura_analog<=385) {boton=7;}
+  if(lectura_analog>=415 && lectura_analog<=425) {boton=8;}
+  if(lectura_analog>=445 && lectura_analog<=460) {boton=9;}
+
+  if(lectura_analog>=1020)  {boton=0; boton_previo = 0;} 
+  if (boton_previo ==boton) {                                           
+    boton_previo= boton;                                              
+    boton_apretado= 0;                                               
+  }  
+
+  else {                                                                
+    boton_previo = boton;                                             
+    boton_apretado = boton;                                        
+  }
+  delay(20);
+  
+ switch (boton_apretado){
+
+  case 1:
+  chip.pu(); chip.set_play( playback_ptr[0],playback_ptr[1]);
+  break;
+
+  case 2:
+  chip.pu(); chip.set_play( playback_ptr[1],playback_ptr[2]);Serial.println("mensaje1");
+  break;
+  
+  case 3:
+  chip.pu(); chip.set_play( playback_ptr[2],playback_ptr[3]);
+  break;
+  
+  case 4:
+  chip.pu(); chip.set_play( playback_ptr[3],playback_ptr[4]);
+  break;
+  
+  case 5:
+  chip.pu(); chip.set_play( playback_ptr[4],playback_ptr[5]);
+  break;
+  
+  case 6:
+  chip.pu(); chip.set_play( playback_ptr[5],playback_ptr[6]);
+  break;
+  
+  case 7:
+  chip.pu(); chip.set_play( playback_ptr[6],playback_ptr[7]);
+  break;
+  
+  case 8:
+  chip.pu(); chip.set_play( playback_ptr[7],playback_ptr[8]);
+  break;
+
+  case 9:
+  chip.pu(); chip.set_play( playback_ptr[8],rec_ptr[8]);Serial.println("mensaje9");
+  break;
+ 
+ }
+
+  
+}
+
+void leer_playbackpointer(){ 
+  
+chip.fwd(); // Esto es porque reseteado o no siempre arranca en el ultimo mensaje grabado con el playback pointer (entonces al avanzarlo vuelve a cero)
+
+  delay(20);
+  rec_ptr[0]= chip.rd_rec_ptr(); 
+  pos =0;
+  //while(playback_ptr[0] != playback_ptr[pos]){ 
+   while(pos<num_msj) {
+    
+    playback_ptr[pos]=chip.rd_play_ptr(); 
+    rec_ptr[pos]= chip.rd_rec_ptr(); 
+    chip.fwd(); 
+    pos++; 
+    if(playback_ptr[pos]=0x010){playback_ptr[pos]=rec_ptr[pos];}
+    delay(50);
+    Serial.println(String(pos));
+    }
+    }
+
+  
+
